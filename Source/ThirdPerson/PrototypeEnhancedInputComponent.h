36f59253c5e46eda5b59168e6959f732c85d133a
virtus2 <khd1323@naver.com>
2023-10-08 17:50:40 +0900

    Create PrototypeEnhancedInputComponent

---
+++ Source/ThirdPerson/PrototypeEnhancedInputComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "PrototypeEnhancedInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSON_API UPrototypeEnhancedInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+};
