a709f7cef005d11f9d90713ceca273311c8238c4
virtus2 <khd1323@naver.com>
2023-11-20 22:48:03 +0900

    feat: 아이템 태그 추가

--- Source/ThirdPerson/PrototypeGameplayTags.cpp
+++ Source/ThirdPerson/PrototypeGameplayTags.cpp
@@ -7,7 +7,9 @@ UE_DEFINE_GAMEPLAY_TAG(TAG_Item_Type_Equipment, "Item.Type.Equipment");
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_Item_Type_Equipment_Armor, "Item.Type.Equipment.Armor");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Item_Type_Equipment_Armor_Helmet, "Item.Type.Equipment.Armor.Helmet");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Item_Type_Equipment_Armor_BodyArmor, "Item.Type.Equipment.Armor.BodyArmor");
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_Item_Type_Equipment_Weapon, "Item.Type.Equipment.Weapon");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Item_Type_Equipment_Weapon_Axe, "Item.Type.Equipment.Weapon.Axe");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Item_Type_Equipment_Weapon_Sword, "Item.Type.Equipment.Weapon.Sword");
 
